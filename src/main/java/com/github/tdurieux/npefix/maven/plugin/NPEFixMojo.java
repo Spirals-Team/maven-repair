a3b80af93837fa705a4b636d3ea4faf88d97c71c
tdurieux <durieuxthomas@hotmail.com>
2017-06-22 06:01:10 +0200

    support different type of selector

--- src/main/java/com/github/tdurieux/npefix/maven/plugin/NPEFixMojo.java
+++ src/main/java/com/github/tdurieux/npefix/maven/plugin/NPEFixMojo.java
@@ -1,9 +1,17 @@
 package com.github.tdurieux.npefix.maven.plugin;
 
+import fr.inria.spirals.npefix.config.Config;
+import fr.inria.spirals.npefix.main.DecisionServer;
 import fr.inria.spirals.npefix.main.all.Launcher;
 import fr.inria.spirals.npefix.resi.CallChecker;
 import fr.inria.spirals.npefix.resi.context.Decision;
+import fr.inria.spirals.npefix.resi.context.Lapse;
 import fr.inria.spirals.npefix.resi.context.NPEOutput;
+import fr.inria.spirals.npefix.resi.selector.ExplorerSelector;
+import fr.inria.spirals.npefix.resi.selector.GreedySelector;
+import fr.inria.spirals.npefix.resi.selector.MonoExplorerSelector;
+import fr.inria.spirals.npefix.resi.selector.RandomSelector;
+import fr.inria.spirals.npefix.resi.selector.Selector;
 import fr.inria.spirals.npefix.resi.strategies.NoStrat;
 import fr.inria.spirals.npefix.resi.strategies.ReturnType;
 import fr.inria.spirals.npefix.resi.strategies.Strat1A;
@@ -44,6 +52,12 @@ import java.util.Locale;
 //@Execute( lifecycle = "surefire", phase = LifecyclePhase.TEST )
 public class NPEFixMojo extends AbstractMojo {
 
+    @Parameter(property = "maven.compiler.source", defaultValue = "1.5")
+    protected String source;
+
+    @Parameter(property = "maven.compile.source", defaultValue = "-1")
+    protected String oldSource;
+
     @Component
     private ArtifactFactory artifactFactory;
 
@@ -56,8 +70,11 @@ public class NPEFixMojo extends AbstractMojo {
     @Parameter( defaultValue = "${project.build.directory}/npefix", property = "resultDir", required = true )
     private File resultDirectory;
 
-    @Parameter( defaultValue = "dom", property = "selector", required = true )
+    @Parameter( defaultValue = "exploration", property = "selector", required = true )
     private String selector;
+
+    @Parameter( defaultValue = "100", property = "laps", required = true )
+    private int nbIteration;
 
     @Parameter(defaultValue="${project}", readonly=true, required=true)
     private MavenProject project;
@@ -103,21 +120,18 @@ public class NPEFixMojo extends AbstractMojo {
         if (!binFolder.exists()) {
             binFolder.mkdirs();
         }
+        int complianceLevel = Integer.parseInt(source.substring(2));
+        if (!oldSource.equals("-1")) {
+            complianceLevel = Integer.parseInt(oldSource.substring(2));
+        }
 
-        Launcher  npefix = new Launcher(sources, outputDirectory.getAbsolutePath() + "/npefix-output", binFolder.getAbsolutePath(), classpath(dependencies));
+        Date initDate = new Date();
+
+        Launcher  npefix = new Launcher(sources, outputDirectory.getAbsolutePath() + "/npefix-output", binFolder.getAbsolutePath(), classpath(dependencies), complianceLevel);
 
         npefix.instrument();
 
-        NPEOutput lapses = npefix.runStrategy(npeTests, new NoStrat(),
-                new Strat1A(),
-                new Strat1B(),
-                new Strat2A(),
-                new Strat2B(),
-                new Strat3(),
-                new Strat4(ReturnType.NULL),
-                new Strat4(ReturnType.VAR),
-                new Strat4(ReturnType.NEW),
-                new Strat4(ReturnType.VOID));
+        NPEOutput lapses = run(npefix, npeTests);
 
 
         spoon.Launcher spoon = new spoon.Launcher();
@@ -129,7 +143,7 @@ public class NPEFixMojo extends AbstractMojo {
         spoon.buildModel();
 
         JSONObject jsonObject = lapses.toJSON(spoon);
-        jsonObject.put("endInit", new Date().getTime());
+        jsonObject.put("endInit", initDate.getTime());
         try {
             for (Decision decision : CallChecker.strategySelector.getSearchSpace()) {
                 jsonObject.append("searchSpace", decision.toJSON());
@@ -138,6 +152,89 @@ public class NPEFixMojo extends AbstractMojo {
         } catch (Exception e) {
             throw new RuntimeException(e);
         }
+    }
+
+    private NPEOutput run(Launcher  npefix, List<String> npeTests) {
+        switch (selector.toLowerCase()) {
+        case "dom":
+            return npefix.runStrategy(npeTests,
+                    new NoStrat(),
+                    new Strat1A(),
+                    new Strat1B(),
+                    new Strat2A(),
+                    new Strat2B(),
+                    new Strat3(),
+                    new Strat4(ReturnType.NULL),
+                    new Strat4(ReturnType.VAR),
+                    new Strat4(ReturnType.NEW),
+                    new Strat4(ReturnType.VOID));
+        case "exploration":
+            return multipleRuns(npefix, npeTests, new ExplorerSelector());
+        case "mono":
+            Config.CONFIG.setMultiPoints(false);
+            return multipleRuns(npefix, npeTests, new MonoExplorerSelector());
+        case "greedy":
+            return multipleRuns(npefix, npeTests, new GreedySelector());
+        case "random":
+            return multipleRuns(npefix, npeTests, new RandomSelector());
+        }
+        return null;
+    }
+
+    private NPEOutput multipleRuns(Launcher  npefix, List<String> npeTests, Selector selector) {
+        DecisionServer decisionServer = new DecisionServer(selector);
+        decisionServer.startServer();
+
+        NPEOutput output = new NPEOutput();
+
+        int countError = 0;
+        while (output.size() < nbIteration) {
+            if(countError > 5) {
+                break;
+            }
+            try {
+                List<Lapse> result = npefix.run(selector, npeTests);
+                if(result.isEmpty()) {
+                    countError++;
+                    continue;
+                }
+                boolean isEnd = true;
+                for (int i = 0; i < result.size() && isEnd; i++) {
+                    Lapse lapse = result.get(i);
+                    if (lapse.getOracle().getError() != null) {
+                        isEnd = isEnd && lapse.getOracle().getError().contains("No more available decision");
+                    } else {
+                        isEnd = false;
+                    }
+                }
+                if (isEnd) {
+                    // no more decision
+                    countError++;
+                    continue;
+                }
+                countError = 0;
+                if(output.size() + result.size() > nbIteration) {
+                    output.addAll(result.subList(0, (nbIteration - output.size())));
+                } else {
+                    output.addAll(result);
+                }
+            } catch (OutOfMemoryError e) {
+                e.printStackTrace();
+                countError++;
+                continue;
+            } catch (Exception e) {
+                if(e.getCause() instanceof OutOfMemoryError) {
+                    countError++;
+                    continue;
+                }
+                e.printStackTrace();
+                countError++;
+                continue;
+            }
+            System.out.println("Multirun " + output.size() + "/" + nbIteration + " " + ((int)(output.size()/(double)nbIteration * 100)) + "%");
+        }
+        output.setEnd(new Date());
+        return output;
     }
 
     private String classpath(List<String> dependencies) {
